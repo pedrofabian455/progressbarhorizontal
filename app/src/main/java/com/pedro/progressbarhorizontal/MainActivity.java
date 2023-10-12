aefbf93b33dcb3e3c5924aab3ca5a2406698a71a
pedrofabian455 <pedrofabianzarate@gmail.com>
2023-10-11 21:30:10 -0300

    progressbarhorizontal implementacion finalizada

---
+++ app/src/main/java/com/pedro/progressbarhorizontal/MainActivity.java
@@ -0,0 +1,43 @@
+package com.pedro.progressbarhorizontal;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    int progess = 0;
+    ProgressBar simpleProgressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        simpleProgressBar = (ProgressBar) findViewById(R.id.simpleprogressbar);
+        Button btniniciar = (Button) findViewById(R.id.btn_iniciar);
+        btniniciar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view ){
+                setProgressValue(progress);
+            }
+        });
+    }
+    private void setProgressValue (final int progress){
+        simpleProgressBar.setProgress(progress);
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                try {
+
+                    Thread.sleep(1000);
+                }catch (interruptedException e){
+                    e.printStackTrace();
+                }
+                setProgressValue(progress + 10);
+            }
+
+        });
+        thread.start();
+    }
+}
